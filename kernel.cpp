142315216ee66421b8357fb5d3bb9e1faa28ab29
Rivercz <hotelsustr1@gmail.com>
2024-10-23 11:02:31 +0200

    Create kernel.cpp
    You need to inject via mapper, there is no licencing from windows

---
+++ kernel.cpp
@@ -0,0 +1,110 @@
+#include <wdf.h>
+#include <ntstrsafe.h>
+
+#define DEVICE_NAME "MyKernelDriver"
+
+typedef struct _DEVICE_EXTENSION {
+    WDFDEVICE Device;
+    WDFQUEUE Queue;
+} DEVICE_EXTENSION, *PDEVICE_EXTENSION;
+
+NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegistryPath)
+{
+    NTSTATUS status;
+    PDEVICE_EXTENSION deviceExtension;
+    WDF_OBJECT_ATTRIBUTES attributes;
+    WDF_DEVICE_INIT_INFO deviceInitInfo;
+    WDFDEVICE device;
+    
+    // Initialize device attributes
+    WDF_OBJECT_ATTRIBUTES_INIT(&attributes);
+    attributes.EvtCleanupCallback = nullptr;
+    attributes.EvtCleanupRoutine = nullptr;
+    attributes.EvtContextCleanup = nullptr;
+    attributes.EvtPnpCallback = nullptr;
+    
+    // Initialize device init info
+    WDF_DEVICE_INIT_INFO_INIT(&deviceInitInfo);
+    deviceInitInfo.Size = sizeof(WDF_DEVICE_INIT_INFO);
+    deviceInitInfo.DriverGuid = GUID_NULL;
+    deviceInitInfo.HwProfileGuid = GUID_NULL;
+    deviceInitInfo.FileName = nullptr;
+    deviceInitInfo.DeviceClass = nullptr;
+    deviceInitInfo.Revision = WDF_DEVICE_INIT_REVISION_1_0;
+    
+    // Create device
+    status = WdfDeviceCreate(&attributes, &deviceInitInfo, &device);
+    if (!NT_SUCCESS(status))
+    {
+        return status;
+    }
+    
+    // Allocate device extension
+    deviceExtension = (PDEVICE_EXTENSION)ExAllocatePoolWithTag(NonPagedPool, sizeof(DEVICE_EXTENSION), DEVICE_NAME);
+    if (deviceExtension == nullptr)
+    {
+        WdfDeviceDelete(device);
+        return STATUS_NO_MEMORY;
+    }
+    
+    // Initialize device extension
+    deviceExtension->Device = device;
+    deviceExtension->Queue = nullptr;
+    
+    // Add device extension to device
+    WdfDeviceSetDeviceExtension(device, deviceExtension);
+    
+    // Create device queue
+    WDF_QUEUE_CONFIG queueConfig;
+    WDF_QUEUE_CONFIG_INIT(&queueConfig);
+    queueConfig.EvtIoCustomQueue = nullptr;
+    queueConfig.EvtIoFileCallback = nullptr;
+    queueConfig.EvtIoDeviceIoControl = nullptr;
+    queueConfig.EvtIoInternalDeviceControl = nullptr;
+    queueConfig.EvtIoOverlapped = nullptr;
+    queueConfig.EvtIoRead = nullptr;
+    queueConfig.EvtIoWrite = nullptr;
+    queueConfig.EvtPnpCallback = nullptr;
+    queueConfig.EvtPowerCallback = nullptr;
+    queueConfig.EvtRequestCallback = nullptr;
+    queueConfig.EvtRequestComplete = nullptr;
+    queueConfig.EvtRequestFail = nullptr;
+    queueConfig.EvtRequestForward = nullptr;
+    queueConfig.EvtRequestRetarget = nullptr;
+    queueConfig.EvtRequestSend = nullptr;
+    queueConfig.EvtRequestSuspend = nullptr;
+    queueConfig.EvtRequestResume = nullptr;
+    queueConfig.EvtRequestCancel = nullptr;
+    queueConfig.EvtRequestCancelSent = nullptr;
+    queueConfig.EvtRequestCancelComplete = nullptr;
+    queueConfig.EvtRequestCompleteWithInformation = nullptr;
+    queueConfig.EvtRequestEndofRequest = nullptr;
+    queueConfig.EvtRequestRestart = nullptr;
+    queueConfig.EvtRequestRestartComplete = nullptr;
+    queueConfig.EvtRequestRestartFailed = nullptr;
+    queueConfig.EvtRequestRestartSent = nullptr;
+    queueConfig.EvtRequestRestartSentComplete = nullptr;
+    queueConfig.EvtRequestRestartSentFailed = nullptr;
+    queueConfig.EvtRequestRestartFailedComplete = nullptr;
+    queueConfig.EvtRequestRestartFailedSent = nullptr;
+    queueConfig.EvtRequestRestartFailedSentComplete = nullptr;
+    queueConfig.EvtRequestRestartFailedSentFailed = nullptr;
+    queueConfig.EvtRequestRestartCompleteSent = nullptr;
+    queueConfig.EvtRequestRestartCompleteSentComplete = nullptr;
+    queueConfig.EvtRequestRestartCompleteSentFailed = nullptr;
+    queueConfig.EvtRequestRestartCompleteFailed = nullptr;
+    queueConfig.EvtRequestRestartCompleteSent = nullptr;
+    queueConfig.EvtRequestRestartCompleteSentComplete = nullptr;
+    queueConfig.EvtRequestRestartCompleteSentFailed = nullptr;
+    queueConfig.EvtRequestRestartCompleteFailed = nullptr;
+    queueConfig.EvtRequestRestartComplete = nullptr;
+    queueConfig.EvtRequestRestart = nullptr;
+    queueConfig.EvtRequestRestartSent = nullptr;
+    queueConfig.EvtRequestRestartSentComplete = nullptr;
+    queueConfig.EvtRequestRestartSentFailed = nullptr;
+    queueConfig.EvtRequestRestartFailedSent = nullptr;
+    queueConfig.EvtRequestRestartFailedSentComplete = nullptr;
+    queueConfig.EvtRequestRestartFailedSentFailed = nullptr;
+    queueConfig.EvtRequestRestartFailedComplete = nullptr;
+    queueConfig.EvtRequestRestartFailedSent = nullptr;
+    queueConfig.EvtRequestRestartFailedSentComplete =
