08da6aeee40d62c5ec2fbba732b6ff0201001f81
Rivercz <hotelsustr1@gmail.com>
2024-10-23 12:34:47 +0200

    Update driver.h

--- driver.h
+++ driver.h
@@ -4,31 +4,55 @@
 #include <intrin.h>
 #include <stdint.h>
 
-// Definice typu funkcí
-typedef __int64(__fastcall* t_CustomFreePool)(__int64, __int64, __int64);
-typedef (__fastcall* MemoryAllocatePages_t)(IN SIZE_T NumberOfBytes, IN ULONG Node);
+typedef __int64(__fastcall* t_FreePool)(__int64, __int64, __int64);
+typedef (__fastcall* AllocatePagesFunc_t)(IN SIZE_T Size, IN ULONG Node);
 
-// Definice struktury Command
-typedef struct {
-    uintptr_t selfref;
-    uintptr_t pid;            // Cílový proces
-    uintptr_t destination;
-    int cmdId;                // Akce k provedení
-    char rw;                  // 0 = čtení, 1 = zápis
-    unsigned char* pSource;
+extern NTSTATUS AdjustRegisters();
+
+// NTSTATUS NTAPI CopyVirtualMemory(PEPROCESS SourceProcess, PVOID SourceAddress, PEPROCESS TargetProcess, PVOID TargetAddress,
+//     SIZE_T BufferSize, KPROCESSOR_MODE PreviousMode, PSIZE_T ReturnSize);
+
+NTKERNELAPI PPEB NTAPI GetProcessPeb(IN PEPROCESS Process);
+NTSYSAPI PVOID RtlGetFileHeader(PVOID PcValue, PVOID* BaseOfImage);
+
+NTSTATUS QuerySystemInfo(
+    _In_ ULONG SystemInfoClass,
+    _Inout_ PVOID SystemInfo,
+    _In_ ULONG SystemInfoLength,
+    _Out_opt_ PULONG ReturnLength
+);
+
+typedef struct
+{
+    uintptr_t selfReference;
+    uintptr_t targetPid;     // PID cílového procesu
+    uintptr_t destinationAddr;
+    int commandId;          // akce k provedení
+    char operationType;     // 0 = čtení, 1 = zápis
+    unsigned char* sourceAddr;
     SIZE_T size;
 } Command;
 
-// Konstanty pro různé příkazy
-#define COMMAND_READWRITE  0xB16B00B5
-#define COMMAND_GETPROCPID 0xBADA55
-#define COMMAND_ISLOADED   0x69420
+#define CMD_READWRITE     0xB16B00B5
+#define CMD_GETPROCPID    0xBADA55
+#define CMD_ISLOADED      0x69420
 
-// Deklarace externích funkcí
-extern NTSTATUS AdjustRegister();
-extern t_CustomFreePool CustomFreePool;
-extern MemoryAllocatePages_t MemoryAllocatePages;
+typedef struct _PROCESS_MODULE_INFO
+{
+    HANDLE SectionHandle;
+    PVOID MappedBaseAddress;
+    PVOID ImageBaseAddress;
+    ULONG ImageSize;
+    ULONG Flags;
+    USHORT LoadOrderIndex;
+    USHORT InitOrderIndex;
+    USHORT LoadCount;
+    USHORT OffsetToFileName;
+    UCHAR FullPathName[256];
+} PROCESS_MODULE_INFO, * PPROCESS_MODULE_INFO;
 
-// Prototypy funkcí
-NTSTATUS InitializeDriver(DWORD32 pid);
-uintptr_t FindPIDByName(char* processName);
+typedef struct _PROCESS_MODULES
+{
+    ULONG ModuleCount;
+    PROCESS_MODULE_INFO Modules[1];
+} PROCESS_MODULES, * PPROCESS_MODULES;
